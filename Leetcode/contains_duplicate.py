839c20227d9f8708cf155b1c08cbf64d6e6e3438
Jin Sunwoo <bora_js@naver.com>
2024-03-28 21:49:25 -0400

    Arrays & Hashing, Easy

--- Leetcode/contains_duplicate.py
+++ Leetcode/contains_duplicate.py
@@ -1,18 +1,18 @@
 from typing import List
 
 class Solution:
+    # brute force
+    # t/c: O(n^2), s/c: O(1)
     def containsDuplicate1(self, nums: List[int]) -> bool:
-        # brute force
-        # t/c: O(n^2), s/c: O(1)
         for i in range(len(nums)-1): 
             for j in range(i+1, len(nums)): 
                 if nums[i] == nums[j]:
                     return True
         return False
     
+    # optimal - hashtable 
+    # t/c: O(n), s/c: O(n)
     def containsDuplicate2(self, nums: List[int]) -> bool:
-        # optimal - hashtable 
-        # t/c: O(n), s/c: O(n)
         hashtable = {}
         for num in nums:
             if num not in hashtable:
@@ -21,9 +21,9 @@ class Solution:
                 return True
         return False
 
+    # optimal - sorting
+    # t/c: O(n logn), s/c: O(1)
     def containsDuplicate3(self, nums: List[int]) -> bool:
-        # optimal - sorting
-        # t/c: O(n logn), s/c: O(1)
         nums.sort()
         for i in range(len(nums)-1):
             if nums[i] == nums[i+1]:
